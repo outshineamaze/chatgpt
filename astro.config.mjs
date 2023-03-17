8071d73d41b3af73255b1420b3421c6c66229646
outshineamaze <outshineamaze@live.com>
2023-03-17 19:39:34 +0800

    fix: fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import solidJs from '@astrojs/solid-js'
 import vercelDisableBlocks from './plugins/vercelDisableBlocks'
 import svelte from "@astrojs/svelte";
 import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/edge'
 import netlify from "@astrojs/netlify/edge-functions";
 
 const envAdapter = () => {
   if (process.env.OUTPUT == 'vercel') {
     return node({
-      mode: 'standalone'
+      mode: 'middleware'
     })
   } else if (process.env.OUTPUT == 'netlify') {
     return netlify()
