297a3ffd0bc18a1d1ea676c185aadddbbbaae1b6
laynezhou <laynezhou@tencent.com>
2023-03-23 22:11:50 +0800

    feat: add dialog

--- src/components/PlayGroud.tsx
+++ src/components/PlayGroud.tsx
@@ -1,25 +1,8 @@
-import { createSignal } from "solid-js";
-import { Button, Modal } from "solid-bootstrap";
-export default () => {
-  const [show, setShow] = createSignal(false);
-  const handleOpen = () => setShow(true);
-  const handleClose = () => setShow(false);
-
+const App = () => {
   return (
     <div>
-      <Button variant="primary" onClick={handleOpen}>Launch demo modal</Button>
-      <Modal show={show()} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Woohoo, you're reading this text in a modal!
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>Close</Button>
-          <Button variant="primary" onClick={handleClose}>Save Changes</Button>
-        </Modal.Footer>
-      </Modal>
+      <button >Show Modal</button>
     </div>
   );
-}
+};
+export default App;
--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,22 +1,28 @@
 import { createSignal, onMount, Show } from 'solid-js'
 import html2canvas from 'html2canvas';
 import { BiRegularScreenshot, BiRegularShare, BiRegularShareAlt } from "solid-icons/bi";
+import { Button, Modal } from "solid-bootstrap";
 // ShareLinkButton component
 const ShareLinkButton = () => {
-  const [hasPass, setHasPass] = createSignal(false)
-  onMount(() => {
-    try {
-      if (localStorage.getItem('pass')) {
-        setHasPass(true)
-      }
-    } catch (err) {
-      console.error(err)
-    }
-  })
+  const [show, setShow] = createSignal(false);
+  const [modalContent, setModalContent] = createSignal("");
+  const handleOpen = (msg) => {
+    setShow(true);
+    setModalContent(msg)
+  }
+  const handleClose = () => {
+    setShow(false);
+    setModalContent("")
+  }
   const copyToClipboard = async () => {
     let finalShareLink = ""
     try {
       const password = localStorage.getItem('pass')
+      if (!password) {
+        if (!password) {
+          handleOpen("未登录用户暂时无法共享访问")
+        }
+      }
       const response = await fetch('/api/gensharelink', {
         method: 'POST',
         headers: {
@@ -31,11 +37,11 @@ const ShareLinkButton = () => {
         const shareLink = responseJson.sharelink;
         finalShareLink = shareLink
         await navigator.clipboard.writeText(shareLink);
-        alert("链接已经复制(分享链接可以问答50次): " + shareLink)
+        handleOpen("链接已经复制(分享链接可以问答50次): " + shareLink)
       }
     } catch (err) {
       if (finalShareLink) {
-        alert("请手动复制(分享链接可以问答50次): " + finalShareLink)
+        handleOpen("请手动复制(分享链接可以问答50次): " + finalShareLink)
       }
       console.error("Failed to copy text: ", err);
     }
@@ -44,6 +50,9 @@ const ShareLinkButton = () => {
     let finalShareLink = ""
     try {
       const password = localStorage.getItem('pass')
+      if (!password) {
+        handleOpen("未登录用户暂时无法分享会话")
+      }
       const messageList = JSON.parse(localStorage.getItem('messageList'))
       if (Array.isArray(messageList) && messageList.length >= 2) {
         const response = await fetch('/api/gensharelink', {
@@ -61,12 +70,12 @@ const ShareLinkButton = () => {
           const shareLink = responseJson.sharelink;
           finalShareLink = shareLink
           await navigator.clipboard.writeText(shareLink);
-          alert("链接已经复制，仅分享当前页面最近的10次对话， 且分享链接内其他用户还能继续对话50次， 链接: " + shareLink)
+          handleOpen("链接已经复制，仅分享当前页面最近的10次对话， 且分享链接内其他用户还能继续对话50次， 链接: " + shareLink)
         }
       }
     } catch (err) {
       if (finalShareLink) {
-        alert("请手动复制， 仅分享当前页面最近的10次对话， 且分享链接内其他用户还能继续对话50次， 链接: " + finalShareLink)
+        handleOpen("请手动复制， 仅分享当前页面最近的10次对话， 且分享链接内其他用户还能继续对话50次， 链接: " + finalShareLink)
       }
       console.error("Failed to copy text: ", err);
     }
@@ -99,20 +108,34 @@ const ShareLinkButton = () => {
       <button onClick={capturePage} sys-edit-btn>
         <BiRegularScreenshot size={24} title="长截图" />截图
       </button>
-      <Show
-        when={hasPass()}
-      >
+ 
         <button onClick={shareSession} sys-edit-btn>
           <BiRegularShare size={24} title="分享会话" />分享会话
         </button>
-      </Show>
-      <Show
-        when={hasPass()}
-      >
+
         <button onClick={copyToClipboard} sys-edit-btn>
           <BiRegularShareAlt size={24} title="分享访问链接" />共享访问
         </button>
-      </Show>
+      <Modal
+        show={show()}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          {
+            modalContent
+          }
+        </Modal.Body>
+        <Modal.Footer>
+        <button onClick={handleClose} sys-edit-btn>
+          确认
+        </button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
